df2ee989dc666871827a3e418723c067a8c2c378
JavierLlanos <javierllanosatoy@gmail.com>
2025-04-02 23:47:03 -0500

    Recuperando cambios

--- main.py
+++ main.py
@@ -1,5 +1,19 @@
+
+from reactpy import component, web, html
+from reactpy.backend.flask import configure
 from flask import Flask
 from datos import productos
 
+mui = web.module_from_template("react", "@mui/material",  fallback=" ⏳")
+Container = web.export(mui, "Container")
+
+@component
+def App():
+    return Container(
+        {"maxWidth": "md"},
+        html.h1 ("Hola, mundo")
+    )
+
 app = Flask(__name__)
+configure(app, App)
 app.run(host='0.0.0.0', debug=True)

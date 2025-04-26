c35dcdc64dee3124836ed43ac7662cfdee6aab8c
Javier Mauricio Llanos Atoy <javierllanosatoy@gmail.com>
2025-04-26 18:04:43 -0500

    actualización

--- main.py
+++ main.py
@@ -6,12 +6,31 @@ from datos import productos
 
 mui = web.module_from_template("react", "@mui/material",  fallback=" ⏳")
 Container = web.export(mui, "Container")
+Grid = web.export(mui, "Grid")
 
 @component
 def App():
     return Container(
         {"maxWidth": "md"},
-        html.h1 ("Hola, mundo")
+        Grid (
+            {"container": True, "spacing": "8"},
+            Grid(
+                {"item": True, "sm": 6, "md": 4, "lg": 3},
+                html.h1("producto 1")
+            ),
+            Grid(
+                {"item": True, "sm": 6, "md": 4, "lg": 3},
+                html.h1("producto 1")
+            ),
+            Grid(
+                {"item": True, "sm": 6, "md": 4, "lg": 3},
+                html.h1("producto 1")
+            ),
+            Grid(
+                {"item": True, "sm": 6, "md": 4, "lg": 3},
+                html.h1("producto 1")
+            ),
+        )
     )
 
 app = Flask(__name__)

9ab9347f144ae3f53a59bb1e4a0124a55f86eb1c
Javier Mauricio Llanos Atoy <javierllanosatoy@gmail.com>
2025-04-26 19:16:42 -0500

    actualización

--- main.py
+++ main.py
@@ -7,12 +7,25 @@ from datos import productos
 mui = web.module_from_template("react", "@mui/material",  fallback=" ⏳")
 Container = web.export(mui, "Container")
 Grid = web.export(mui, "Grid")
+Paper = web.export(mui, "Paper")
+Box = web.export(mui, "Box")
 
 def tarjetas(productos):
     def tarjeta(producto):
-        return Grid (
+        return Grid(
             {"item": True, "sm": 6, "md": 4, "lg": 3},
-            html.h1(producto['nombre'])
+            Paper(
+                {"elevation": 4},
+                html.img({
+                    "src": "https://picsum.photos/id{producto['id']}/400/100",
+                    "class_name": "img-fluid",
+                    "style": { "width": "100%", "height": "5rem"}
+                }),
+                Box(
+                    {"bgcolor":"background.paper"},
+                    html.h1(producto['nombre'])
+                ),
+            )
         )
     
     return Grid (
